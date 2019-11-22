7ec67d69b67850388391f3c31e5113b6462dc5ea
Yashu Mittal <mittalyashu77@gmail.com>
2019-11-22 22:02:34 +0530

    Commit message, fields, footer, timestamp and color

--- dist/index.js
+++ dist/index.js
@@ -890,35 +890,62 @@ const axios = __webpack_require__(53)
 
 async function run() {
   try {
-    console.log("github.context");
-    console.log(github.context.payload);
     const github_context = github.context.payload
 
     const discord_webhook_url = core.getInput('webhook_url', { required: true });
-
     const os = core.getInput('os', { required: true });
     const node_version = core.getInput('node_version', { required: true });
+    const build_status = core.getInput('build_status');
+    const workflow_name = core.getInput('workflow_name', { required: true });
 
-    console.log(github_context.head_commit.message.split("\n\n"))
+    let build_status_color;
+    if (build_status) {
+      return build_status_color = 0x2cbe4e;
+    } else {
+      return build_status_color = 0xcb2431;
+    }
+
+    const commit_message = github_context.head_commit.message.split("\n\n")
+    const commit_message_title = commit_message[0]
+    const commit_message_description = commit_message[1]
 
     const data = {
-      content: "Hello world!",
-      username: "GitHub Actions",
+      username: workflow_name,
       avatar_url: "https://i.imgur.com/u6mj8bs.png",
       embeds: [
         {
-          title: github_context.head_commit.message,
-          url: `https://www.github.com/${github_context.repository.organization}/${github_context.repository.name}/commit/${github_context.head_commit.id}`,
+          title: commit_message_title,
+          url: github_context.head_commit.url,
+          description: !!commit_message_description ? commit_message_description : '',
+          author: {
+            name: github_context.sender.login,
+            icon_url: github_context.sender.avatar_url,
+            url: github_context.sender.html_url
+          },
+          color: !!build_status_color ? build_status_color : 0x202225,
           fields: [
             {
-              name: "OS",
+              name: "Branch",
               value: os
+            },
+            {
+              name: "Commit ID",
+              value: github_context.head_commit.id
+            },
+            {
+              name: "OS",
+              value: os,
+              inline: true
             },
             {
               name: "Node version",
               value: node_version
             }
-          ]
+          ],
+          timestamp: Date.now(),
+          footer: {
+            text: 'GitHub Action'
+          }
         }
       ]
     };
