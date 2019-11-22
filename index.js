64f8b99dce4e92de2b136d44377fe60339ac65f9
Yashu Mittal <mittalyashu77@gmail.com>
2019-11-22 19:19:58 +0530

    Update payload & console log github.context

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+const github = require('@actions/github');
 const core = require('@actions/core')
 const axios = require('axios')
 
 async function run() {
-  try { 
+  try {
+    console.log("github.context");
+    console.log(github.context);
+
     const discord_webhook_url = core.getInput('webhook_url', { required: true });
 
     const data = {
-      content: "Hello world!"
+      content: "Hello world!",
+      username: "GitHub Actions",
+      avatar_url: "https://i.imgur.com/u6mj8bs.png"
     };
 
     axios.post(discord_webhook_url, data)
