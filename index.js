954b062d675c026be28841da35c716e5b6295eca
Yashu Mittal <mittalyashu77@gmail.com>
2019-11-22 19:37:08 +0530

    Accept os & node_version inputs

--- index.js
+++ index.js
@@ -8,11 +8,18 @@ async function run() {
     console.log(github.context);
 
     const discord_webhook_url = core.getInput('webhook_url', { required: true });
+    const os = core.getInput('os', { required: true });
+    const node_version = core.getInput('node_version', { required: true });
 
     const data = {
       content: "Hello world!",
       username: "GitHub Actions",
-      avatar_url: "https://i.imgur.com/u6mj8bs.png"
+      avatar_url: "https://i.imgur.com/u6mj8bs.png",
+      embeds: [
+        {
+          title: `${os} v${node_version}`
+        }
+      ]
     };
 
     axios.post(discord_webhook_url, data)
